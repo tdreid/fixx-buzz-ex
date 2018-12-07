775c4c89f9c6f4d344e6eb5bc698af5fa40b35d7
tdreid <trevor.d.reid@gmail.com>
2018-12-07 20:26:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Assorted solutions to the [Fizz Buzz Test](http://wiki.c2.com/?FizzBuzzTest)
---
+++ js/fizz-buzz.js
@@ -0,0 +1,13 @@
+function fizzBuzz(n) {
+  if (n % 3 == 0 && n % 5 == 0) {
+    return 'FizzBuzz';
+  } else if (n % 3 == 0) {
+    return 'Fizz';
+  } else if (n % 5 == 0) {
+    return 'Buzz';
+  } else {
+    return n;
+  }
+}
+
+[...new Array(100).keys()].forEach(n => console.log(`${fizzBuzz(n + 1)}`));
