51deb4377450d9454723d7d742a118ad3adbd953
Trevor Reid <trevor.d.reid@gmail.com>
2019-01-26 21:14:45 -0500

    Create FizzBuzz extension method for int value type

---
+++ cs/FizzBuzzExtensionMethod/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzzExtension
+    {
+        public static string FizzBuzz(this int n)
+        {
+            string substitute = String.Format(
+                "{0}{1}",
+                n % 3 == 0 ? "Fizz" : String.Empty,
+                n % 5 == 0 ? "Buzz" : String.Empty);
+            return string.IsNullOrEmpty(substitute) ? n.ToString() : substitute;
+        }
+    }
+}
